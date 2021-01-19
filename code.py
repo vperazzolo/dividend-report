4ce028d95f41d300e769a4f39f5782662be65ef3
vperazzolo <vperazzolo@babbel.com>
2021-01-19 10:24:03 +0100

    transforming type and format of Data column

--- code.py
+++ code.py
@@ -2,15 +2,16 @@
 import pandas as pd
 import numpy as np
 div_report = pd.read_excel("/Users/vbarros/Documents/Analysis/PythonProject/ProventosRecebidos.xlsx")
-#print(div_report)
+#print(div_report.head(50))
 
-#find dates with "," and ", " and remove them.
+#find dates with "," and ", " and remove them. (Can be skipped once to_datetime function automatically does that)
 wrong_date = div_report["Data"]
 fix_date = wrong_date.str.contains(",")
-
 div_report["Data"] = wrong_date.str.replace(",","")
 div_report["Data"] = wrong_date.str.replace(" ","")
 
-print(div_report["Data"].head(50))
+#Convert type (object to datetime) and format (MM/DD/YYYY to YYYY-MM-DD) of Data column.
+div_report["Data"] = pd.to_datetime(div_report["Data"])
 
-#next step-> convert type  and format of Data column
+
+print(div_report["Data"].head(50))
